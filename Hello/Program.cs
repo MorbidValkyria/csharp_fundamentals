47171891f50af7cac4cd243097b44e8147616290
Francisco <francisco.vergarat@gmail.com>
2019-07-31 07:21:51 -0400

    Added comments to the Hello project

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,8 +4,17 @@ namespace Hello
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+            Multi-line comment
+            Everyhthing enclosed here
+            is ignored by the compiler
+         */
+
+         // Single line comment
+        static void Main(string[] args) // Main entry point
         {
+            // Console.WriteLine() Prints text to the screen, then reads from a new line
+            // Which means it prints a an empty line after the text (\n)
             Console.WriteLine("Hello World!");
         }
     }
